1a010b5de38ebd954dc82c79babe010a144f6b11
Kantulaev <Kantulaev@gmail.com>
2022-12-16 17:48:44 +0600

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+/* A way to make sure that the code is compatible with the browser. */
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
+};
+
+export default config;
